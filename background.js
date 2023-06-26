7773a59b3ac2bbe275adf3956938794bbd165ba7
Samuel Mediani <66485729+SamMed05@users.noreply.github.com>
2023-06-26 12:50:43 +0200

    format code

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 let data = {};
 
 function updateBadge() {
-  chrome.tabs.query({}, function(tabs) {
+  chrome.tabs.query({}, function (tabs) {
     let tabCount = tabs.length;
     chrome.action.setBadgeText({ text: tabCount.toString() });
     chrome.storage.local.set({ tabCount: tabCount });
@@ -13,8 +13,8 @@ chrome.tabs.onRemoved.addListener(updateBadge);
 chrome.tabs.onUpdated.addListener(updateBadge);
 
 // Increment total tab count
-chrome.tabs.onCreated.addListener(function(tab) {
-  chrome.storage.local.get(null, function(result) {
+chrome.tabs.onCreated.addListener(function (tab) {
+  chrome.storage.local.get(null, function (result) {
     data = result;
     data.totalTabs = (data.totalTabs || 0) + 1;
     chrome.storage.local.set(data);
@@ -22,45 +22,56 @@ chrome.tabs.onCreated.addListener(function(tab) {
 });
 
 // This function only runs once when the extension is installed or updated
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.local.get(null, function(result) {
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.local.get(null, function (result) {
     data = result;
     data.totalTabs = data.totalTabs || 0;
 
     // Initialize tab count to the current number of tabs in the browser
-    chrome.tabs.query({}, function(tabs) {
+    chrome.tabs.query({}, function (tabs) {
       data.tabCount = tabs.length;
-      chrome.action.setBadgeText({text: data.tabCount.toString()});
+      chrome.action.setBadgeText({ text: data.tabCount.toString() });
       chrome.storage.local.set(data);
     });
   });
 });
 
 // Listen for the message and respond to it
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "countTabs") {
     updateTabAndWindowCounts();
 
     tabCount = 0;
     windowCount = 0;
 
-    chrome.windows.getAll({populate: true}, function(windows) {
+    chrome.windows.getAll({ populate: true }, function (windows) {
       windowCount = windows.length;
 
-      windows.forEach(function(window) {
+      windows.forEach(function (window) {
         tabCount += window.tabs.length;
       });
 
       let date = new Date().toLocaleDateString();
 
-      chrome.storage.local.get(null, function(result) {
+      chrome.storage.local.get(null, function (result) {
         data = result;
-        data[date] = {tabs: tabCount, windows: windowCount};
+        data[date] = { tabs: tabCount, windows: windowCount };
 
-        chrome.storage.local.set({totalTabs: data.totalTabs, [date]: data[date], windows: windowCount}, function() {
-          // Send a response to the popup with the updated data
-          sendResponse({tabCount: tabCount, windowCount: windowCount, result: result});
-        });
+        chrome.storage.local.set(
+          {
+            totalTabs: data.totalTabs,
+            [date]: data[date],
+            windows: windowCount,
+          },
+          function () {
+            // Send a response to the popup with the updated data
+            sendResponse({
+              tabCount: tabCount,
+              windowCount: windowCount,
+              result: result,
+            });
+          }
+        );
       });
     });
 
--- popup.js
+++ popup.js
@@ -1,23 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Initialize variables and DOM elements
-  const tabCountElem = document.getElementById('tab-count');
-  const windowCountElem = document.getElementById('window-count');
-  const totalTabsCell = document.getElementById('all-time-tabs');
-  const resetButton = document.getElementById('resetButton');
-  const inputDays = document.getElementById('numOfDays');
-  const ctx = document.getElementById('chart').getContext('2d');
+  const tabCountElem = document.getElementById("tab-count");
+  const windowCountElem = document.getElementById("window-count");
+  const totalTabsCell = document.getElementById("all-time-tabs");
+  const resetButton = document.getElementById("resetButton");
+  const inputDays = document.getElementById("numOfDays");
+  const ctx = document.getElementById("chart").getContext("2d");
   let chart;
 
   // Get the tab and window count and update the UI
   function countTabs() {
-    chrome.runtime.sendMessage({action: "countTabs"}, function(response) {
+    chrome.runtime.sendMessage({ action: "countTabs" }, function (response) {
       updateTable(response.tabCount, response.windowCount, response.result);
       renderChart(response.result);
     });
   }
-  
+
   // Add event listener to the input element to update the chart
-  inputDays.addEventListener('change', function(response) {
+  inputDays.addEventListener("change", function (response) {
     chrome.storage.local.set({ numOfDays: inputDays.value }); // Store the input value in local storage
     countTabs();
   });
@@ -29,14 +29,14 @@ document.addEventListener('DOMContentLoaded', function() {
     let windowCounts = [];
     let now = new Date();
     let numOfDays = parseInt(inputDays.value);
-  
+
     for (let i = numOfDays - 1; i >= 0; i--) {
       let date = new Date(now);
       date.setDate(now.getDate() - i);
-      dates.push(date.toLocaleDateString('en-GB'));
-      let data = result[date.toLocaleDateString()] || {tabs: 0, windows: 0};
+      dates.push(date.toLocaleDateString("en-GB"));
+      let data = result[date.toLocaleDateString()] || { tabs: 0, windows: 0 };
       tabCounts.push(data.tabs || 0);
-      windowCounts.push(data.windows || 0);  
+      windowCounts.push(data.windows || 0);
     }
 
     // Destroy existing chart instance if it exists
@@ -47,29 +47,32 @@ document.addEventListener('DOMContentLoaded', function() {
     chart = new Chart(ctx, {
       data: {
         labels: dates,
-        datasets: [{
-          type: 'line',
-          label: 'Tab count',
-          data: tabCounts,
-          borderColor: 'rgb(26, 115, 232)',
-          borderWidth: 2,
-          fill: true,
-          backgroundColor: 'rgba(0, 109, 204, 0.3)',
-          pointRadius: 3,
-          tension: 0.1,
-          hidden: false
-        }, {
-          type: 'bar',
-          label: 'Window count',
-          data: windowCounts,
-          borderColor: 'rgb(242, 86, 58)',
-          borderWidth: 2,
-          backgroundColor: 'rgba(242, 86, 58, 0.4)',
-          order: 2,
-          barThickness: 20,
-          borderRadius: 50,
-          hidden: false
-        }]
+        datasets: [
+          {
+            type: "line",
+            label: "Tab count",
+            data: tabCounts,
+            borderColor: "rgb(26, 115, 232)",
+            borderWidth: 2,
+            fill: true,
+            backgroundColor: "rgba(0, 109, 204, 0.3)",
+            pointRadius: 3,
+            tension: 0.1,
+            hidden: false,
+          },
+          {
+            type: "bar",
+            label: "Window count",
+            data: windowCounts,
+            borderColor: "rgb(242, 86, 58)",
+            borderWidth: 2,
+            backgroundColor: "rgba(242, 86, 58, 0.4)",
+            order: 2,
+            barThickness: 20,
+            borderRadius: 50,
+            hidden: false,
+          },
+        ],
       },
       options: {
         scaleShowValues: true,
@@ -77,17 +80,17 @@ document.addEventListener('DOMContentLoaded', function() {
         scales: {
           x: {
             ticks: {
-              color: "black"
-            }
+              color: "black",
+            },
           },
           y: {
             beginAtZero: true,
             ticks: {
-              color: "black"
-            }
-          }
-        }
-      }
+              color: "black",
+            },
+          },
+        },
+      },
     });
   }
 
@@ -99,15 +102,15 @@ document.addEventListener('DOMContentLoaded', function() {
     totalTabsCell.innerText = result.totalTabs || 0; // Update the totalTabsCell with the totalTabs count, if it exists
 
     // Reset the totalTabs count if the reset button is clicked
-    resetButton.addEventListener('click', function() {
-      chrome.storage.local.set({totalTabs: 0}, function() {
+    resetButton.addEventListener("click", function () {
+      chrome.storage.local.set({ totalTabs: 0 }, function () {
         totalTabsCell.textContent = 0;
       });
     });
   }
 
   // Retrieve the input value from local storage
-  chrome.storage.local.get({ numOfDays }, function(result) {
+  chrome.storage.local.get({ numOfDays }, function (result) {
     inputDays.value = result.numOfDays;
     countTabs();
   });
